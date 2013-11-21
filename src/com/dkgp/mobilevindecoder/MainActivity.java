36d4764143602257ed607e65282e4c3aad2f89bc
gpatel2 <gpatel2@gmail.com>
2013-11-20 22:26:38 -0600

    Image capture added. MainActivity code refractored

--- src/com/dkgp/mobilevindecoder/MainActivity.java
+++ src/com/dkgp/mobilevindecoder/MainActivity.java
@@ -1,8 +1,16 @@
 package com.dkgp.mobilevindecoder;
 
+import java.io.File;
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
@@ -10,13 +18,15 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private final int _scanRequest = 0;
+	private final int _cameraRequest = 1;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
- // test from GP
-	
+ 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -28,22 +38,68 @@ public class MainActivity extends Activity {
 		Intent intent = new Intent("com.google.zxing.client.android.SCAN");
 		intent.putExtra("SCAN_MODE", "ONE_D_MODE");
 		startActivityForResult(intent, 0);
-		
 	}
 
-	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		   if (requestCode == 0) {
-		      if (resultCode == RESULT_OK) {
-		         String contents = intent.getStringExtra("SCAN_RESULT");
-		         String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
-		         // Handle successful scan
-		         EditText editText = (EditText)findViewById(R.id.scannedVIN);
-		         editText.setText("VIN: "+contents +" format: " + format);
-		         
-		      } else if (resultCode == RESULT_CANCELED) {
-		         // Handle cancel
-		      }
-		   }
+	public void takePicture(View view) {
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		
+		File imageFile = getImageFile();
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imageFile));
+		startActivityForResult(intent, _cameraRequest);
+	}
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == Activity.RESULT_OK){
+			switch (requestCode){
+			case _scanRequest:
+				String contents = data.getStringExtra("SCAN_RESULT");
+		        String format = data.getStringExtra("SCAN_RESULT_FORMAT");
+		        // Handle successful scan
+		        EditText editText = (EditText)findViewById(R.id.scannedVIN);
+		        editText.setText("VIN: "+contents +" format: " + format);
+				break;
+			case _cameraRequest:
+				handleCameraRequest(requestCode, resultCode, data);
+				break;
+				
+				default:
+					Log.e("onActivityResult", String.format("Unrecognized request code: %d", requestCode));
+			}
+		} else {
+			Log.e("onActivityResult", String.format("Request Failed - resultCode: %d, requestCode: %d", resultCode, requestCode));
 		}
+	}
+	
+	protected void handleCameraRequest(int requestCode, int resultCode, Intent data){
+		Bitmap image;
+		
+		if (data != null){
+			image = (Bitmap)data.getParcelableExtra("data");
+		}
+		else {
+			File imageFile = getImageFile();
+			String imageFileName = imageFile.getAbsolutePath();
+			image = BitmapFactory.decodeFile(imageFileName);
+		}
+		int height = image.getHeight();
+		int width = image.getWidth();
+		Log.e("handleCameraRequest", String.format("Image size ..  height:%d width:%d", height, width));
+		
+	}
+	
+	private File getImageFile() {
+		File targetDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+		assureThatDirectoryExist(targetDir);
+		File imageFile = new File(targetDir, "MyPicture.jpg");
+		
+		return imageFile;
+	}
+	
+	private void assureThatDirectoryExist(File directory) {
+		if (!directory.exists()){
+			directory.mkdirs();
+		}
+		
+	}
 
 }
