dc5e85b59ff79e373819458cea92be2593e1706a
gpatel2 <gpatel2@gmail.com>
2013-11-27 12:58:47 -0600

    barcode image added

--- gen/com/dkgp/mobilevindecoder/R.java
+++ gen/com/dkgp/mobilevindecoder/R.java
@@ -47,12 +47,13 @@ public final class R {
         public static final int standard_padding=0x7f080000;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int launcher_icon=0x7f020001;
-        public static final int no_image=0x7f020002;
-        public static final int share_via_barcode=0x7f020003;
-        public static final int shopper_icon=0x7f020004;
-        public static final int vehicle=0x7f020005;
+        public static final int barcode=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int launcher_icon=0x7f020002;
+        public static final int no_image=0x7f020003;
+        public static final int share_via_barcode=0x7f020004;
+        public static final int shopper_icon=0x7f020005;
+        public static final int vehicle=0x7f020006;
     }
     public static final class id {
         public static final int action_settings=0x7f09003b;
