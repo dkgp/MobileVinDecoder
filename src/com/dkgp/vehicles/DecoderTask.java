b71909b5ad10336ab0cabbb1e1ebafa99a2364e4
dkgp <david.kwak@adp.com>
2013-11-29 21:51:34 -0600

    moved hard coded url settings to android settings.

--- src/com/dkgp/vehicles/DecoderTask.java
+++ src/com/dkgp/vehicles/DecoderTask.java
@@ -3,13 +3,16 @@ package com.dkgp.vehicles;
 import org.json.JSONObject;
 
 import android.app.ProgressDialog;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.widget.EditText;
 
 public class DecoderTask extends AsyncTask<String, String, JSONObject> {
 	private ProgressDialog pDialog;
 	private MainActivity _activity;
-	private static String url = "https://api.dev-2.cobalt.com/inventory/rest/v1.0/vehicles/detail?inventoryOwner=gmps-kindred&locale=en_us";
+	//private static String url = "https://api.dev-2.cobalt.com/inventory/rest/v1.0/vehicles/detail?inventoryOwner=gmps-kindred&locale=en_us";
 
 	public DecoderTask(MainActivity mainActivity) {
 		_activity = mainActivity;
@@ -36,6 +39,11 @@ public class DecoderTask extends AsyncTask<String, String, JSONObject> {
 		String request = "{\"vehicles\":[{\"vehicle\":{\"vin\":\""
 				+ barcode + "\"}}]}";
 
+		Resources res = _activity.getResources();
+		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(_activity);
+		String apiUrl = sharedPref.getString(res.getString(R.string.api_url), "");
+		String vinuploadApi = sharedPref.getString(res.getString(R.string.api_vin_decode), "");
+		String url=apiUrl +"?inventoryOwner="+ vinuploadApi;
 		JSONParser jParser = new JSONParser();
 		JSONObject json = jParser.getJSONFromUrl(url, request);
 		return json;
--- src/com/dkgp/vehicles/UploadVehicleTask.java
+++ src/com/dkgp/vehicles/UploadVehicleTask.java
@@ -3,7 +3,10 @@ package com.dkgp.vehicles;
 import org.json.JSONObject;
 
 import android.app.ProgressDialog;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -11,7 +14,7 @@ import android.widget.Toast;
 public class UploadVehicleTask extends AsyncTask<String, String, JSONObject> {
 	private ProgressDialog pDialog;
 	private MainActivity _activity;
-	private static String url = "https://api.dev-2.cobalt.com/inventory/rest/v1.0/vehicles?inventoryOwner=gmps-kindred";
+	//private static String url = "https://api.dev-2.cobalt.com/inventory/rest/v1.0/vehicles?inventoryOwner=gmps-kindred";
 
 	public UploadVehicleTask(MainActivity mainActivity) {
 		_activity = mainActivity;
@@ -38,11 +41,18 @@ public class UploadVehicleTask extends AsyncTask<String, String, JSONObject> {
 	    //barcode = "1HGCM82633A004352";
 //		String request = "{\"vehicles\":[{\"vehicle\":{\"vin\":\""
 //				+ barcode + "\"}}]}";
+		Resources res = _activity.getResources();
+		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(_activity);
+		String apiUrl = sharedPref.getString(res.getString(R.string.api_url), "");
+		String vinuploadApi = sharedPref.getString(res.getString(R.string.api_create_vehicle), "");
+		String url=apiUrl +"?inventoryOwner="+ vinuploadApi;
+		
 		String make ="";
 		String model="";
 		String year="";
 		String vin="";
 		String dealerPhotoId ="";
+		
 		String request ="{\"criteria\":{\"vehicleContexts\":[{\"vehicleContext\":{\"vehicle\":{\"make\":{\"label\":\""+make+"\"},\"model\":{\"label\":\""+model+"\"},\"year\":"+year+",\"vin\":\""+ vin +"\",\"assets\":{\"dealerPhotos\":[{\"id\":\""+dealerPhotoId+"\"}]}},\"modifiedFields\":[\"make.label\",\"model.label\",\"vin\",\"year\",\"assets\"]}}],\"inventoryOwner\":\"gmps-kindred\"}}";
 		request ="{\"criteria\":{\"vehicleContexts\":[{\"vehicleContext\":{\"vehicle\":{\"make\":{\"label\":\"Volkswagen\"},\"model\":{\"label\":\"Jetta Sedan\"},\"year\":2009,\"vin\":\"3vwal71k99m128066\",\"assets\":{\"dealerPhotos\":[{\"id\":\"7242888004\"}]}},\"modifiedFields\":[\"make.label\",\"model.label\",\"vin\",\"year\",\"assets\"]}}],\"inventoryOwner\":\"gmps-kindred\"}}";
 
